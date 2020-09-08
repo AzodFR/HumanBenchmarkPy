8f16936e9c8630398b5b77a00a8f88c9a5a402b2
Azod <azod.contact@gmail.com>
2020-09-08 18:15:40 +0200

    init

---
+++ Reaction_Time.py
@@ -0,0 +1,22 @@
+#################################################################################################
+#                                                                                               #
+#   Author: Azod                                                                                #
+#   Github: github.com/AzodFR                                                                   #
+#   Comment: Just launch the script, strat the test and place the cursor on the red color       #
+#                                                                                               #  
+#################################################################################################
+import pyautogui as pg
+
+i = 0
+
+while True:
+    mouse = pg.position()
+    if mouse.x == 0:
+        break
+    elif str(pg.pixel(mouse.x, mouse.y)) == "(75, 219, 106)":
+        pg.click()
+        print("Green !")
+        i+=1
+    if i == 5:
+        break
+        
